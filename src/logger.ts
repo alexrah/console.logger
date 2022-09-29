131284b3601727b998db7a4069f8e09662cfc2e7
alessandro stoppato <alexrah@gmail.com>
2022-09-29 14:46:12 +0200

    src/logger.ts:shuffle() replace loose comparison != with strict comparison !==

--- src/logger.ts
+++ src/logger.ts
@@ -143,7 +143,7 @@ class logger  {
         let currentIndex = array.length,  randomIndex;
 
         // While there remain elements to shuffle.
-        while (currentIndex != 0) {
+        while (currentIndex !== 0) {
 
             // Pick a remaining element.
             randomIndex = Math.floor(Math.random() * currentIndex);
